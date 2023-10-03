bdbe4dd1a8134fcd480e984bd4e794287c420a44
JerichoGavino <145358392+JerichoGavino@users.noreply.github.com>
2023-10-03 22:39:03 +0800

    PetRecord.java

--- factoryPattern/PetRecord.java
+++ factoryPattern/PetRecord.java
@@ -4,6 +4,8 @@ public class PetRecord {
     private String petId;
     private String petName;
     private Pet pet;
+    private String breed;
+    private int NoofLives;
 
     public String getPetId() {
         return petId;
@@ -28,4 +30,30 @@ public class PetRecord {
     public void setPet(Pet pet) {
         this.pet = pet;
     }
+    
+		
+	
+
+	public void setBreed(String breed) {
+		// TODO Auto-generated method stub
+		this.breed = breed;
+		
+	}
+
+	public void setNoofLives(int noofLives) {
+		NoofLives = noofLives;
+		
+	}
+
+	public String getBreed() {
+		// TODO Auto-generated method stub
+		return breed;
+	}
+
+	public int getNoofLives() {
+		// TODO Auto-generated method stub
+		return NoofLives;
+	}
+    
+   
 }
