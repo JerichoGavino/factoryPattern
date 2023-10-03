ceaaddfa3dbf2cad3b0c35e351d87b3fc40163b0
JerichoGavino <145358392+JerichoGavino@users.noreply.github.com>
2023-10-03 22:36:27 +0800

    Cat.java

--- factoryPattern/Cat.java
+++ factoryPattern/Cat.java
@@ -2,14 +2,17 @@ package factoryPattern;
 
 public class Cat implements Pet {
     private Integer noOfLives;
-
+ 
+    @Override
+    public void setNoOfLives(Integer noOfLives) {
+        this.noOfLives = noOfLives;
+    }
+    
     public Integer getNoOfLives() {
         return noOfLives;
     }
 
-    public void setNoOfLives(Integer noOfLives) {
-        this.noOfLives = noOfLives;
-    }
+   
 
     @Override
     public String makeSound() {
@@ -19,5 +22,13 @@ public class Cat implements Pet {
     @Override
     public String play() {
         return "Catching/pursuing moving objects!";
-    }
+    
+	
+	}
+
+	@Override
+	public void setBreed(String breed) {
+		// TODO Auto-generated method stub
+		
+	}
 }
