bcd1d700fb8b180262a18ee9c5f5d9ba0fd41576
JerichoGavino <145358392+JerichoGavino@users.noreply.github.com>
2023-10-03 22:38:09 +0800

    Pet.java

--- factoryPattern/Pet.java
+++ factoryPattern/Pet.java
@@ -3,4 +3,8 @@ package factoryPattern;
 public interface Pet {
     public String makeSound();
     public String  play();
+	void setBreed(String breed);
+	void setNoOfLives(Integer noOfLives);
+   
+    
 }
